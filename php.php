81b1dc1b2a43e25bd0d3fc663d9cde8805b2c320
Irfan Sarwar Khan <irfan.sarwar.khan30@gmail.com>
2022-11-21 10:53:31 +0400

    two sum with php

---
+++ php.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $flippedArr = array_flip($nums);
+        
+        foreach ($nums as $key => $num) {
+            $num2 = $target - $num;
+            if (isset($flippedArr[$num2]) && $key != $flippedArr[$num2]) {
+                return [$key, $flippedArr[$num2]];
+            }
+        }
+        
+        return [];
+    }
+?>
