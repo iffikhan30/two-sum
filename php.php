349af6ab4c39c6c8e81b3d8c9b200d15f9226482
Irfan Sarwar Khan <irfan.sarwar.khan30@gmail.com>
2022-11-29 04:53:49 +0000

    remove lines

--- php.php
+++ php.php
@@ -6,14 +6,12 @@
  */
     function twoSum($nums, $target) {
         $flippedArr = array_flip($nums);
-        
         foreach ($nums as $key => $num) {
             $num2 = $target - $num;
             if (isset($flippedArr[$num2]) && $key != $flippedArr[$num2]) {
                 return [$key, $flippedArr[$num2]];
             }
         }
-        
         return [];
     }
 ?>
